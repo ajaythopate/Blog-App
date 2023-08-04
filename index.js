9ad414f5689bfbfdc4a9e5863b4d80c70b462d20
ajaythopate <ajaythopate999@gmail.com>
2023-08-04 20:57:01 +0530

    API added

---
+++ index.js
@@ -0,0 +1,9 @@
+//Fetch blogs from the API
+
+function fetchBlogs() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(data => {
+            
+        }
+}
