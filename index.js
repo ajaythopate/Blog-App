7af1954bb453eea3be00c48a3841c95fd88c60e6
ajaythopate <ajaythopate999@gmail.com>
2023-08-04 21:00:20 +0530

    API added

--- index.js
+++ index.js
@@ -4,6 +4,29 @@ function fetchBlogs() {
     fetch('https://jsonplaceholder.typicode.com/posts')
         .then(response => response.json())
         .then(data => {
-            
-        }
+            const blogesContainer = document.getElementById('blogsContainer'); 
+            blogsContainer.innerHTML = '';
+
+            data.forEach(blog => {
+                const blogCard = document.createElement('div');
+                blogCard.classList.add('blogCard');
+
+                const blogTitle = document.createElement('h2');
+                blogTitle.textContent = blog.title;
+
+                const blogContent = document.createElement('p');
+                blogContent.textContent = blog.body;
+
+                const deleteButton = document.createElement('button');
+                deleteButton.textContent = 'Delete';
+                deleteButton.classList.add('deleteButton');
+
+                blogCard.appendChild(blogTitle);
+                blogCard.appendChild(blogContent);
+                blogCard.appendChild(deleteButton);
+
+                blogsContainer.appendChild(blogCard);
+            });
+        });
 }
+fetchBlogs();
